b5368929d6d3c635979d18f1abf473b9cb7f35c4
Karandeep Singh <karandeep070104@gmail.com>
2025-03-14 02:41:20 +0530

    Basic Shortner

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/karan-0701/url-shortner
+
+go 1.22.5
+
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/lib/pq v1.10.9
+)
--- main.go
+++ main.go
@@ -2,39 +2,48 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
 	"time"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 )
 
 const base62Chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
+var db *sql.DB
+
 func generateUniqueNumber() int64 {
 	return time.Now().UnixNano()
 }
+
 func connectDB() (*sql.DB, error) {
 	dsn := "user=username password=password dbname=mydb sslmode=disable"
 	db, err := sql.Open("postgres", dsn)
 	if err != nil {
 		return nil, err
 	}
-
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
 	return db, nil
 }
 
 func checkEntry(db *sql.DB, table string, column string, value string) (bool, error) {
-	query := fmt.Sprintf("SELECT 1 FROM %s WEHERE %s = $1 LIMIT 1", table, column)
+	query := fmt.Sprintf("SELECT 1 FROM %s WHERE %s = $1 LIMIT 1", table, column)
 	var exists int
 	row := db.QueryRow(query, value)
-	// store 1 in exts if there exist a row in the database
 	err := row.Scan(&exists)
-
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return false, nil
 		}
 		return false, err
 	}
-
 	return true, nil
 }
 
@@ -42,16 +51,13 @@ func toBase62(num int64) string {
 	if num == 0 {
 		return "0"
 	}
-
 	base := int64(len(base62Chars))
 	result := ""
-
 	for num > 0 {
 		remainder := num % base
 		result = string(base62Chars[remainder]) + result
 		num /= base
 	}
-
 	return result
 }
 
@@ -64,16 +70,71 @@ func genRandNum(db *sql.DB, table string, column string) (string, error) {
 	num := generateBase62ID()
 	res, err := checkEntry(db, table, column, num)
 	if err != nil {
-		return " ", nil
+		return "", err
 	}
-
-	if res == true {
+	if res {
 		return genRandNum(db, table, column)
 	}
-
 	return num, nil
 }
 
+func Shorten(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "Invalid Request", http.StatusBadRequest)
+		return
+	}
+	longUrl := r.FormValue("url")
+	if longUrl == "" {
+		http.Error(w, "URL is required", http.StatusBadRequest)
+		return
+	}
+	shortCode, err := genRandNum(db, "urls", "short_code")
+	if err != nil {
+		http.Error(w, "Failed to generate short code", http.StatusInternalServerError)
+		return
+	}
+	_, err = db.Exec("INSERT INTO urls (short_code, original_url) VALUES ($1, $2)", shortCode, longUrl)
+	if err != nil {
+		http.Error(w, "Failed to save URL", http.StatusInternalServerError)
+		return
+	}
+	response := map[string]string{
+		"short_url": fmt.Sprintf("http://localhost:8080/%s", shortCode),
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+func Redirect(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	shortCode := vars["shortCode"]
+	var longUrl string
+	query := db.QueryRow("SELECT original_url FROM urls WHERE short_code = $1", shortCode)
+	err := query.Scan(&longUrl)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			http.Error(w, "URL not found", http.StatusNotFound)
+		} else {
+			http.Error(w, "Database error", http.StatusInternalServerError)
+		}
+		return
+	}
+	http.Redirect(w, r, longUrl, http.StatusMovedPermanently)
+}
+
 func main() {
+	var err error
+	db, err = connectDB()
+	if err != nil {
+		log.Fatal("Failed to connect to the database:", err)
+	}
+	defer db.Close()
+
 	r := mux.NewRouter()
+	r.HandleFunc("/shorten", Shorten).Methods("POST")
+	r.HandleFunc("/{shortCode}", Redirect).Methods("GET")
+
+	log.Println("Server started on :8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
